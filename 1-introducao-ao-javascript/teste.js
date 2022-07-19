4934b46c3e6183a649a223dda8f11f5f9a176af3
AndersonRodriguess <anderson.rodriguess@outlook.com.br>
2022-07-18 21:27:06 -0300

    introdução de aprendizado ao javascript

---
+++ 1-introducao-ao-javascript/teste.js
@@ -0,0 +1,23 @@
+// Fazer comentário é com o uso da barras dupla para comentar uma linha.
+// Para comentar várioas linhas tem que usar /**/ e escrever seu comentários de "n" linhas entre os asteristicos.
+
+//Uso da CONSTANTE é parecido com da Variavel, só que não pode ter seu valor alterado e precisam ser inicializadas
+
+var desconto = 0.2;
+const PRECO = 2; //Por boas práticas, constante são declaras sempre em caixa alta.
+var total = PRECO - desconto;
+
+//FUNÇÕES
+
+function soma (a, b) {
+    return a + b;
+}
+
+//chamando a função
+//Colocando nome da função e entre parenteses colocando os valores
+soma(3, 5);
+
+/*Quando quisermos conversar com o console, utilizamos o seguinte código:
+console.log("") 
+Entre os parentes e aspas digitamos o que queremos ver no console */
+
