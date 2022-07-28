69798b8e0498e1a6307ba214abdf92f573de70e1
AndersonRodriguess <anderson.rodriguess@outlook.com.br>
2022-07-28 15:15:08 -0300

    Atividade prática sobre módulo em JavaScript

---
+++ 9-modulos-em-JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Anderson", 30));
+console.log(mostraCidade("Anderson", "Rio de Janeiro"));
+console.log(mostraHobby("Anderson", "Jogos"));
